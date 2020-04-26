d2ed8a70e1b502955a05092370818fcea01ab52a
evgen <eugeniyglushkov@gmail.com>
2020-04-26 22:34:55 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@
 
 var contains = function (arr, elem) {
     let cont = false;
-    for (let i = 0;i < arr.length;i++) {
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i] === elem) {
             cont = true;
         }
-    };
+    }
+    ;
     return cont;
 }
 
 class Area {
 
-    constructor(areaId) {
+    constructor(areaId, outerClickHandler, winHandler) {
         this.areaId = areaId;
         this.firstOpenField = null;
         this.fields = [
@@ -37,9 +38,14 @@ class Area {
             '&#x1F43F;&#xFE0F;',
         ];
         this.previous = [null, null];
+        this.outerClickhandler = outerClickHandler;
+        this.winHandler = winHandler;
+        this.openedPairs = 0;
     }
 
     clickHandler(field) {
+        this.outerClickhandler();
+        
         if (this.firstOpenField === null && !contains(this.previous, field) && !field.isBlocked) {
             if (this.previous[0] !== null && !this.previous[0].isBlocked && !this.previous[1].isBlocked) {
                 this.previous[0].flip();
@@ -57,6 +63,12 @@ class Area {
             field.flip();
 
             if (field.emoji === this.firstOpenField.emoji) {
+                this.openedPairs++;
+
+                if (this.openedPairs == 6) {
+                    this.winHandler();
+                }
+
                 field.isBlocked = true;
                 this.firstOpenField.isBlocked = true;
                 this.firstOpenField.flipper.querySelector('.back').classList.add('green_back');
@@ -105,21 +117,25 @@ class Area {
         }
     }
 
-    newGame = function () {
+    newGame() {
         this.removeBacks();
         this.fieldsInitiate();
+        this.openedPairs = 0;
+        this.fields.forEach(field => {
+            field.close();
+            field.unblock();
+        });
+        this.firstOpenField = null;
+        this.previous = [null, null];
     }
 }
 
 class Field {
 
     constructor(fieldId, clickHandler) {
-        let f = document.querySelector('#' + fieldId);
-        let fl = f.querySelector('.flipper');
-        this.fieldId = fieldId;
-        this.field = f;
+        this.field = document.querySelector('#' + fieldId);
         this.clickHandler = clickHandler;
-        this.flipper = fl;
+        this.flipper = document.querySelector('#' + fieldId).querySelector('.flipper');
         this.flipper.addEventListener('click', this.click.bind(this));
         this.isBlocked = false;
         this.emoji = null;
@@ -149,8 +165,123 @@ class Field {
     isBlocked() {
         return this.isBlocked;
     }
+
+    close() {
+        this.field.querySelector('.flipper').classList.remove('flip');
+    }
+
+    unblock() {
+        this.isBlocked = false;
+    }
 }
 
-var area = new Area('game-place');
-area.newGame();
+class Timer {
+    constructor(timerId, handler) {
+        this.minutes = 0;
+        this.seconds = 0;
+        this.timerElem = document.querySelector('#' + timerId);
+        this.updateTimerElem();
+        this.handler = handler;
+        this.timerId = null;
+    }
+
+    digitalString(num) {
+        let digString = '' + num;
+
+        if (num <= 9) {
+            digString = 0 + digString;
+        }
+
+        return digString;
+    }
+
+    set(min, sec) {
+        this.minutes = min;
+        this.seconds = sec;
+        this.updateTimerElem();
+    }
+
+    start() {
+        this.timerId = setInterval(this.tick.bind(this), 1000);
+    }
+
+    tick() {
+        if (this.minutes > 0 || this.seconds > 0) {
+            if (this.seconds === 0) {
+                this.minutes--;
+                this.seconds = 59;
+            } else {
+                this.seconds--;
+            }
+
+            if (this.seconds === 0 && this.minutes === 0) {
+                this.handler();
+            }
+
+            this.updateTimerElem();
+        } else {
+            this.stop();
+        }
+    }
+
+    stop() {
+        if (this.timerId !== null && this.timerId !== undefined) {
+            clearInterval(this.timerId);
+        }
+    }
+
+    reset() {
+        this.stop();
+        this.minutes = 0;
+        this.seconds = 0;
+        this.updateTimerElem();
+    }
+
+    updateTimerElem() {
+        this.timerElem.innerHTML = this.digitalString(this.minutes) + ':' + this.digitalString(this.seconds);
+    }
+}
+
+class Game {
+    
+    constructor() {
+        this.area = new Area('game-place', this.clickCardOnStart.bind(this), this.winHandler.bind(this));
+        this.timer = new Timer('timer', this.loseHandler.bind(this));
+        this.isStarted = false;
+        document.querySelector('#newGame').addEventListener('click', this.new.bind(this));
+    }
+
+    new() {
+        this.timer.set(1, 0);
+        this.area.newGame();
+        document.querySelector('.popup').style.display = 'none';
+    }
+    
+    clickCardOnStart() {
+        if (!this.isStarted) {
+            this.isStarted = true;
+            this.timer.start();
+        }
+    }
+
+    winHandler() {
+        document.querySelector('.popup').style.display = '';
+        document.querySelector('#popup__title').innerHTML = 'Win';
+        document.querySelector('#newGame').innerHTML = 'Play again';
+        this.timer.reset();
+        this.isStarted = false;
+
+    }
+
+    loseHandler() {
+        document.querySelector('.popup').style.display = '';
+        document.querySelector('#popup__title').innerHTML = 'Lose';
+        document.querySelector('#newGame').innerHTML = 'Try again';
+        this.timer.reset();
+        this.isStarted = false;
+    }
+}
+
+var game = new Game();
+game.new();
 
